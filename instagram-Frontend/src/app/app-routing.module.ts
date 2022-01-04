037b2b027cba7b06436e5a139462c0c3243e45c6
Abhishek Singh Tanwar <abhishek.tanwar@cybergindia.local>
2022-01-04 13:58:46 +0530

    added signup page

--- instagram-Frontend/src/app/app-routing.module.ts
+++ instagram-Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedsComponent } from './feeds/feeds.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'header',component:HeaderComponent}, 
+  {path:'feeds',component:FeedsComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- instagram-Frontend/src/app/app.module.ts
+++ instagram-Frontend/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LoginComponent } from './login/login.component';
 import { FeedsComponent } from './feeds/feeds.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { FeedsComponent } from './feeds/feeds.component';
     AppComponent,
     HeaderComponent,
     LoginComponent,
-    FeedsComponent
+    FeedsComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
